d7b4d1dc1aa41bf6c19f7eb11a2b310ebec19ccc
Alaruthe <akionishimura60@gmail.com>
2022-09-13 23:34:08 -0700

    I am Tired

--- week4/pages/api/hello.js
+++ week4/pages/api/hello.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+import path from 'path';
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+const dataDir = path.join( process.cwd(), "data" );
+
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const filepath = path.join( dataDir, "persons.json" );
+
+  const jsondata = fs.readFileSync( filepath, "utf8" );
+
+  const jsonObj = JSON.parse( jsondata );
+
+  jsonObj.sort(
+    function(a,b){
+      return a.name.localeCompare(b.name);
+    }
+  );
+
+  res.status(200).json( jsonObj );
 }
